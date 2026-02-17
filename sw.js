1f8e237f1d7839b608a1958581e6511ad88b96c7
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-17 23:07:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
-const CACHE_NAME = 'laporin-v6';
+const CACHE_NAME = 'laporin-v8';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/appwrite@13.0.1',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./index.html', './manifest.json'])));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
