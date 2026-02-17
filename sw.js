61c67dd9a4a8472b1caa9ac325d1f9cb0bf3baf3
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-18 00:39:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-const CACHE_NAME = 'pajakpro-v3';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE = 'pajakpro-vfinal';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/appwrite@13.0.1',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', e => {
