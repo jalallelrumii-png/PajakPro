46899e43eb92cbbf091e47ed76016284dee3e0d6
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-17 23:29:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const CACHE_NAME = 'laporin-v11';
+const CACHE = 'laporin-vfinal';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json'])));
+});
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
