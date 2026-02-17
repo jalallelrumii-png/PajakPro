871eb4673b4b8f190c0ab4df79b500feadb1280a
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-17 23:13:12 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,14 @@
-const CACHE_NAME = 'laporin-v8';
-const FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.jsdelivr.net/npm/appwrite@13.0.1',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-  'https://cdn.jsdelivr.net/npm/chart.js'
-];
+const NAME = 'laporin-v10';
+const FILES = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  e.waitUntil(caches.open(NAME).then(c => c.addAll(FILES)));
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== NAME).map(k => caches.delete(k)))));
 });
